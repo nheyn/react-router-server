a7a7c4b4157d62e0d199d99f16869494e115f455
nheyn <nick@heyn.io>
2015-06-15 16:04:41 -0600

    Create Skelton for Route Parser Test
    Added a list of test todo on a Route Parser.

---
+++ __tests__/routeParser-test.js
@@ -0,0 +1,21 @@
+describe('RouteParser' () => {
+	it('finds api handlers', () => {
+		//TODO
+	});
+
+	it('finds static file handlers', () => {
+		//TODO
+	});
+
+	it('removes api and static file handlers', () => {
+		//TODO
+	});
+
+	it('creates express routes from api handlers', () => {
+		//TODO
+	});
+
+	it('creates express routes from static file handlers', () => {
+		//TODO
+	});
+});

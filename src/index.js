866c83409d7ed27a9433051972ac49625e8af50a
nheyn <nick@heyn.io>
2015-06-22 16:12:49 -0600

    Added Constructor Function For the Server
    Added function to index.js that will create the server (as an express app or router).

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 /**
  * @flow
  */
-var routeToRouter = require('./routeToRouter.js');
+var ReactRouterServer = require('./server');
+var routeToRouter = require('./routeToRouter');
+
+/*------------------------------------------------------------------------------------------------*/
+//	--- Constructor Function ---
+/*------------------------------------------------------------------------------------------------*/
+/**
+ * Create an express Router that uses the given route and calls the given callback for each request.
+ *
+ * @param route				{ReactRouterRoute}	The route for this server
+ * @param [initialCallback]	{ExpressCallback}	The first callback to call for this server
+ *
+ * @return					{ExpressRouter}		The router for this server
+ */
+function createRouter(route: ReactRouterRoute, initialCallback?: ExpressCallback): ExpressRouter {
+	var server = new ReactRouterServer(route, initialCallback);
+	return server.getRouter();
+}
+
+/**
+ * Create an express Router that uses the given route and calls the given callback for each app.
+ *
+ * @param route				{ReactRouterRoute}	The route for this server
+ * @param [initialCallback]	{ExpressCallback}	The first callback to call for this server
+ *
+ * @return	{ExpressApp}	The app for this server
+ */
+function createApp(route: ReactRouterRoute, initialCallback?: ExpressCallback): ExpressApp {
+	var server = new ReactRouterServer(route, initialCallback);
+	return server.getApp();
+}
 
 /*------------------------------------------------------------------------------------------------*/
 //	--- Exports ---
 /*------------------------------------------------------------------------------------------------*/
 module.exports.RouteParser = require('./routeParser');
+module.exports.server = { createRouter, createApp };
 module.exports.makeApiFunctionHandler = routeToRouter.makeApiFunctionHandler;
 module.exports.makeApiRouterHandler = routeToRouter.makeApiRouterHandler;
 module.exports.makeStaticFileHandler = routeToRouter.makeStaticFileHandler;
